828e7bdc745d04dd835370de54fccce41c53ae9c
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-03-21 11:41:52 +0100

    Update login.php

--- login.php
+++ login.php
@@ -23,35 +23,7 @@
 </head>
 
 <body>
-  <?php
-require('db.php');
-session_start();
-// If form submitted, insert values into the database.
-if (isset($_POST['email'])){
-        // removes backslashes
-  $email = stripslashes($_REQUEST['email']);
-        //escapes special characters in a string
-  $email = mysqli_real_escape_string($con,$email);
-  $password = stripslashes($_REQUEST['password']);
-  $password = mysqli_real_escape_string($con,$password);
-  //Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE email='$email'
-and password='$password'";
-  $result = mysqli_query($con,$query) or die(mysql_error());
-        $row = mysqli_fetch_assoc($result);
-  $rows = mysqli_num_rows($result);
-        if($rows==1){
-      $_SESSION['username'] = $email;
-            $_SESSION['profits'] = $row['profits'];
-            // Redirect user to index.php
-      header("Location: dashboard.php");
-         }else{
-  echo "<div class='form'>
-<h3>Email or Password is incorrect.</h3>
-<br/>Click here to <a href='login.php'>Login</a></div>";
-  }
-    }else{
-?>
+ 
 
 
   <div class="container-scroller">
