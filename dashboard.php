af96e10ad8e148096e5c2b42f3a012a45185cf82
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-03-22 10:34:23 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -50,7 +50,7 @@ include("auth.php");
       <div class="navbar-menu-wrapper d-flex align-items-top"> 
         <ul class="navbar-nav">
           <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
-            <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold"><?php echo $_SESSION['username']; ?></span></h1>
+            <h1 class="welcome-text">Good Day, <span class="text-black fw-bold"><?php echo $_SESSION['username']; ?></span></h1>
             <h3 class="welcome-sub-text">Your personalized dashboard </h3>
           </li>
         </ul>
